a3b0d1d0728150403b089bc7b6f78c2184bfd5c3
Glory R. Nayak <mailtogloryrnayak567@gmail.com>
2025-11-19 22:13:46 +0530

    Update Days 13-15: Combining Tables (INNER JOIN, LEFT JOIN, Multiple Joins).sql
    Added Day 15 challenge question

--- Days 13-15: Combining Tables (INNER JOIN, LEFT JOIN, Multiple Joins).sql
+++ Days 13-15: Combining Tables (INNER JOIN, LEFT JOIN, Multiple Joins).sql
@@ -97,3 +97,30 @@ GROUP BY
     SS.SERVICE
 ORDER BY 
     WEEKS_PRESENT DESC;
+
+/* 
+==================================================
+ DAY 14:  Multiple Joins
+==================================================
+*/
+-- **Question:** Create a comprehensive service analysis report for week 20 showing: service name, total patients admitted 
+-- that week, total patients refused,average patient satisfaction, count of staff assigned to service, and count of staff 
+-- present that week. Order by patients admitted descending.
+SELECT
+	 sw.week ,
+    sw.service,
+    sw.patients_admitted AS total_admitted,
+    sw.patients_refused AS total_refused,
+    sw.patient_satisfaction AS avg_satisfaction,
+    COUNT(DISTINCT s.staff_id) AS total_staff_assigned,
+    COUNT(CASE WHEN ss.present = 1 THEN 1 END) AS staff_present
+FROM services_weekly sw
+LEFT JOIN staff s 
+    ON sw.service = s.service
+LEFT JOIN staff_schedule ss
+    ON s.staff_id = ss.staff_id
+    AND sw.week = ss.week
+WHERE sw.week = 20
+GROUP BY sw.service, sw.patients_admitted, sw.patients_refused, sw.patient_satisfaction
+ORDER BY total_admitted DESC;
+
